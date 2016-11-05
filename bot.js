1b4c66213a2a2ce28446b117b1b4d0a2092ed3f3
Arsene Lakpa <aclak1@yahoo.com>
2016-11-05 08:43:04 -0500

    updated bot

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@
 var Twit = require('twit');
 
 // A String I Created to tweet
-var message = "This is a test Tweet!";
+var message = "Hakuna Matata!";
 
 // We need to include our configuration file
 var T = new Twit(require('./config.js'));
@@ -18,6 +18,63 @@ T.post('statuses/update', { status: message }, function(err, data, response) {
 
 }
 
-tweetMessage(); // Run the function!
+// This function selects a tweet to respond to
+var tweetId;
+function selectTweet(query) {
+    var tweets = T.stream('statuses/filter', {track: query}, function(stream) {
+      stream.on('data', function(tweet) {
+        console.log(tweet.text);
+      });
+      stream.on('error', function(error) {
+        console.log(error);
+      });
+    });
+
+    //TODO Store the ID of this tweet
+    //TODO Store the ID of a random tweet to respond to
+    //TODO Parse through that tweet (possibly write a helper function?)
+    //TODO Conduct a google search and reply to the tweet
+
+    //console.log(tweetId);
+    //id = tweetData[Math.random() * 10].id_str;
+}
+
+//tweetMessage(); // Run the function!
+console.log((90071992547409921).toString());
+//selectTweet("leukemia");
 
 setInterval(tweetMessage, 1000 * 60 * 60); // Tweets the message every hour
+
+//Goog Stuffz
+
+// Our (custom) Google library
+var google = require('google');
+
+//Symptom Google Searcher
+function symSearch(query) {
+    google.resultsPerPage = 25
+    var nextCounter = 0
+
+    google(query, function (err, res){
+      if (err) console.error(err)
+
+      for (var i = 0; i < res.links.length; ++i) {
+        var link = res.links[i];
+        console.log(link.title + ' - ' + link.href)
+        console.log(link.description + "\n")
+      }
+
+      if (nextCounter < 4) {
+        nextCounter += 1
+        if (res.next) res.next()
+      }
+    })
+}
+
+symSearch('milk');
+
+
+/*Late Nite Notes: Make the bot read the words in the tweet. If any of the words are on our 
+list of "trouble words" then add that word to our google search query. This will (maybe) provide
+provide us with a more accurate diagnosis.
+*/
