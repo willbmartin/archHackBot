60cb6fbdda5caab92f197d55d7b073acc239cea4
William Martin <williammartin@gatech.edu>
2016-11-05 16:27:39 -0400

    organized, added runner, output of search

--- bot.js
+++ bot.js
@@ -1,24 +1,30 @@
-// This Example shows how to tweet a simple message.
+
+/*Late Nite Notes: Make the bot read the words in the tweet. If any of the words are on our 
+list of "trouble words" then add that word to our google search query. This will (maybe) provide
+provide us with a more accurate diagnosis.
+*/
 
 
 
+var Twit = require('twit'); // Twitter library
+var T = new Twit(require('./config.js')); // config file; api keys
+var google = require('google'); // Our (custom) Google library
 
-// Our Twitter library
-var Twit = require('twit');
+setInterval(tweetMessage, 1000 * 60 * 30); // runs the code every 30 min
 
-// A String I Created to tweet
-var message = "Hakuna Matata!";
-
-// We need to include our configuration file
-var T = new Twit(require('./config.js'));
+function runner() {
+  //retweetLatest("#test");
+  //findSickTweet();
+  //tweetMessage("boop im a bot");
+  symSearch("headache");
+  console.log(myResults);
+}
 
 // This function tweets the String in the variable message
 function tweetMessage(message) {
-
-T.post('statuses/update', { status: message }, function(err, data, response) {
-  		console.log(data)
- })
-
+  T.post('statuses/update', { status: message }, function(err, data, response) {
+  		console.log(data);
+  })
 }
 
 // This function selects a tweet to respond to
@@ -44,7 +50,15 @@ function selectTweet(query) {
 
 
 
-function retweetLatest(myTag) {
+// function findSickTweet() {
+//   var randIndex = Math.floor((Math.random() * sickList.length));
+  
+//   var tagSearch = {q: sickList[randIndex], count: 10, result_type: "recent"}; 
+//   T.get('search/tweets', tagSearch, function (error, data) {
+//   // log out any errors and responses
+// }
+
+function retweetLatest(myTag) {  
 var tagSearch = {q: myTag, count: 10, result_type: "recent"}; 
 T.get('search/tweets', tagSearch, function (error, data) {
   // log out any errors and responses
@@ -54,63 +68,82 @@ T.get('search/tweets', tagSearch, function (error, data) {
     // ...then we grab the ID of the tweet we want to retweet...
   var retweetId = data.statuses[0].id_str;
   // ...and then we tell Twitter we want to retweet it!
-  T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
-    if (response) {
-      console.log('Success! Check your bot, it should have retweeted something.')
-    }
-    // If there was an error with our Twitter call, we print it out here.
-    if (error) {
-      console.log('There was an error with Twitter:', error);
-    }
-  })
+    T.post('statuses/retweet/' + retweetId, { }, function (error, response) {
+      if (response) {
+        console.log('Success! Check your bot, it should have retweeted something.')
+      }
+      // If there was an error with our Twitter call, we print it out here.
+      if (error) {
+        console.log('There was an error with Twitter:', error);
+      }
+    })
   }
   // However, if our original search request had an error, we want to print it out here.
-  else {
-    console.log('There was an error with your hashtag search:', error);
-  }
-});
+    else {
+      console.log('There was an error with your hashtag search:', error);
+    }
+  });
 }
 
-//tweetMessage(); // Run the function!
-//console.log((90071992547409921).toString());
-//selectTweet("leukemia");
 
-setInterval(tweetMessage, 1000 * 60 * 60); // Tweets the message every hour
-
-//
-
-}
-//Goog Stuffz
-
-// Our (custom) Google library
-var google = require('google');
 
 //Symptom Google Searcher
+var myResults;
 function symSearch(query) {
-    google.resultsPerPage = 25
+    google.resultsPerPage = 3
     var nextCounter = 0
+    var treatmentResults = ["","",""];
 
-    google(query, function (err, res){
+
+    google(query + " treatment", function (err, res){
       if (err) console.error(err)
 
-      for (var i = 0; i < res.links.length; ++i) {
+
+      var forCount = 0
+      var grammar = ''
+      for (var i = 0; i < 3; ++i) {
+        if(0 == forCount) {
+          grammar = 'First '
+        } else if (1 == forCount) {
+          grammar = 'Second '
+        } else {
+          grammar = 'Third '
+        }
         var link = res.links[i];
-        console.log(link.title + ' - ' + link.href)
-        console.log(link.description + "\n")
+        var finalResult = grammar + 'result: ' + link.title + ' - ' + link.href;
+        //console.log(finalResult);
+        treatmentResults[forCount] = link.title + ' - ' + link.href;
+        forCount += 1
+      }
+      for (var j = 0; j < treatmentResults.length; j++) {
+        if (treatmentResults[j].includes("WebMD")) {
+          console.log(treatmentResults[j]);
+          
+          tweetMessage(treatmentResults[j]);
+          return null;
+        }
+
       }
 
-      if (nextCounter < 4) {
-        nextCounter += 1
-        if (res.next) res.next()
+      for (var j = 0; j < treatmentResults.length; j++) {
+        if (!treatmentResults[j].includes("null")) {
+          console.log(treatmentResults[j]);
+          tweetMessage(treatmentResults[j]);
+          return null;
+        }
       }
+
+      //To go to next page of results: 
+
+      // if (nextCounter < 4) {
+      //   nextCounter += 1
+      //   if (res.next) res.next()
+      // }
     })
 }
 
-//symSearch('milk');
-retweetLatest("medicine");
-//checkDM();
 
-/*Late Nite Notes: Make the bot read the words in the tweet. If any of the words are on our 
-list of "trouble words" then add that word to our google search query. This will (maybe) provide
-provide us with a more accurate diagnosis.
-*/
+
+
+
+runner();
