6d5f4781ea1d1b156b3f35ac2eea96e2d388b543
juliepierides <juliepierides@gatech.edu>
2016-11-04 21:10:45 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key:         'mUmhgjBDuInygsEi2it1jZnT9',
+  consumer_secret:      '3ri6j4hJQqap1tpy6ulBxK5bjXL76qSv4xqy3B7fmpIS8kAXZL',
+  access_token:         '794712379686813696-RWgxnnQduXFVaWSve3bTJxaQl4aD6YQ',
+  access_token_secret:  'GBTAk7IrtrHgrIIdWThDpMM5xNkySsUOu1Dbmse7lobac'
+}
